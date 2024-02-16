fce571a2c2f266cf3139f0a40082736229411311
Sen1ch66 <arsenzahovajko@gmail.com>
2024-02-16 16:51:24 +0200

    все найс

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const treebuild = document.querySelector("button")
-treebuild.style = "padding: 5px 10px;"
-const input = document.querySelector("input")
-input.style.padding = '10px 20px'
-treebuild.addEventListener("click", () => {
-    const height = document.querySelector("input").value
+const treebuil = document.querySelector("button")
+treebuil.style = "padding: 5px 10px; "
+const clear = document.querySelector(".clearButton")
+const input = document.querySelector("input").style = 'padding:10px 20px; outline: 1px solid red;'
+const treeContainer = document.querySelector(".treeContainer")
+treebuil.addEventListener("click", () => {
+    const height = document.querySelector("input").value;
+    treebuil.disabled = true;
     function treebuild (){
         for(let w = 1; w <= height; w++){
             const treediv = document.createElement("div")
-            const treeContainer = document.querySelector(".treeContainer")
             treeContainer.appendChild(treediv)
             treediv.classList = "treeblocks"
-            treediv.style.width = 400 - w*50 + 'px';
+            treediv.style.width = 400 - 300/w + 'px';
+            clear.addEventListener("click", () => treediv.style = "display: none;")
         }
     }
     if(height<= 0){
@@ -23,3 +25,4 @@ treebuild.addEventListener("click", () => {
         treebuild();
     }
 })
+
